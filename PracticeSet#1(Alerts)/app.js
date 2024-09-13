00e02d2ad50c4bdfa71e54869bbc6b2f4262f804
HammadMustafa <hammadworks123@gmail.com>
2024-09-13 23:52:35 +0500

    first commit

---
+++ PracticeSet#1(Alerts)/app.js
@@ -0,0 +1,14 @@
+// 1))
+alert("1) Welcome to our website");
+// 2))
+alert("2) Error! Please Enter a valid Password");
+// 3))
+alert("3) Welcome to JS Land... \n Happy coding!");
+
+// 4))
+alert("4) Welcome to JS Land...")
+var result = prompt("Happy Coding!\n\nPrevent this page from creating additional dialogs.");
+
+
+//5
+console.log("5) Hello I can run Js through my web browser console.")
---
+++ PracticeSet#2(Variables and Strings)/app.js
@@ -0,0 +1,45 @@
+// 1))
+var username ;
+
+
+// 2))
+let myName="Hammad Mustafa"
+
+
+
+// 3))
+var message="Hello World";
+alert(message);
+
+
+// 4))Bio Data
+
+let studName="Hammad Mustafa"
+let studAge=20
+let studProfesion="Becoming Full Stack Developer"
+
+alert(`4) The student's name is ${studName}`);
+alert(`The student's age is ${studAge}`);
+
+console.log(`The student's profesion is ${studProfesion}`);
+
+// 5))
+let Var= "PIZZA \n PIZZ \n PIZ \n PI \n P "
+
+alert("5) \n \n" + Var )
+
+
+// 6))
+var email="mi715234@gmail.com";
+alert("6) \n \n  My email address is " + email);
+
+
+// 7))
+
+var book="A smarter way to learn JavaScript."
+
+alert("7) \n \n I am trying to learn form the Book : " + book);
+
+
+// 9))
+alert("9)) \n \n ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
