c4187af2b54c68ac5f3aa66292f3b5f20fa815d8
HammadMustafa <hammadworks123@gmail.com>
2024-10-26 15:46:56 +0500

    First Commit

---
+++ PasswordGenerator/app.js
@@ -0,0 +1,10 @@
+const randomValues = "ABCSWHDAUERM!@#$%^&^%$#32145278175asandjhrwdasuasdmumasas";
+
+    function generatePassword() {
+        let randomPass = "";
+        for (let i = 0; i < 10; i++) {
+            const randomNum = Math.floor(Math.random() * randomValues.length);
+            randomPass += randomValues[randomNum];
+        }
+        document.getElementById("passwordDisplay").value = randomPass;
+    }
